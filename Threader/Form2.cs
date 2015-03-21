a84339f670886b1d7050eba393c71e62c0799bb1
Jack Smith <rabbitskynet@gmail.com>
2015-03-21 12:15:47 +0300

    commit

---
+++ Threader/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Threader
+{
+	public partial class Form2 : Form
+	{
+		public Form2()
+		{
+			InitializeComponent();
+		}
+
+		private void Form2_Load(object sender, EventArgs e)
+		{
+			this.Icon = Threader.Properties.Resources.threadicon;
+		}
+
+		
+		private void button1_Click(object sender, EventArgs e)
+		{
+			this.Hide();
+		}
+
+		private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			e.Cancel = true;
+			this.Hide();
+		}
+	}
+}
